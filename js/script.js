0a3006d414998f9860fc1f04a986010c99654c6e
SiAce <siacespark@gmail.com>
2019-08-08 19:35:28 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,58 +1,68 @@
 
 function loadData() {
+  /*
+  The $ that shows up in variable names, like $body for example, is just a character like any other. In this case, it refers to the fact that the variable referenced by $body is a jQuery collection, not a DOM node.
+  */
+  const $body = $('body');
+  const $wikiElem = $('#wikipedia-links');
+  const $nytHeaderElem = $('#nytimes-header');
+  const $nytElem = $('#nytimes-articles');
+  const $greeting = $('#greeting');
 
-    /*
-    The $ that shows up in variable names, like $body for example, is just a character like any other. In this case, it refers to the fact that the variable referenced by $body is a jQuery collection, not a DOM node.
-    */
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
+  // clear out old data before new request
+  $wikiElem.text('');
+  $nytElem.text('');
 
-    // clear out old data before new request
-    $wikiElem.text("");
-    $nytElem.text("");
+  const streetStr = $('#street').val();
+  const cityStr = $('#city').val();
+  const address = `${streetStr}, ${cityStr}`;
 
-    var streetStr = $('#street').val();
-    var cityStr = $('#city').val();
-    var address = streetStr + ', ' + cityStr;
+  $greeting.text(`So, you want to live at ${address}?`);
 
-    $greeting.text('So, you want to live at ' + address + '?');
+  // load nytimes
+  const nytimesUrl = `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${cityStr}&sort=newest&api-key=7vzH5AwW66mGFFZrXo68um2QcC02ywTA`;
+  $.getJSON(nytimesUrl, (data) => {
+    $nytHeaderElem.text(`New York Times Articles About ${cityStr}`);
 
-
-    // load streetview
-    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
-    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
-
-
-    // load nytimes
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=3abc9a3d23e60b38c21b4ab9b0a91c07:17:69911633'
-    $.getJSON(nytimesUrl, function(data){
-
-        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
-
-        articles = data.response.docs;
-        for (var i = 0; i < articles.length; i++) {
-            var article = articles[i];
-            $nytElem.append('<li class="article">'+
-                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
-                '<p>' + article.snippet + '</p>'+
-            '</li>');
-        };
-
-    }).error(function(e){
-        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
-    });
+    const articles = data.response.docs;
+    for (let i = 0; i < articles.length; i++) {
+      const article = articles[i];
+      $nytElem.append(`<li class="article">
+        <a href="${article.web_url}">${article.headline.main}</a>
+        <p>${article.snippet}</p>
+        </li>`);
+    }
+  }).error(() => {
+    $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+  });
 
 
+  // load wikipedia data
+  const wikiUrl = `http://en.wikipedia.org/w/api.php?action=opensearch&search=${cityStr}&format=json&callback=wikiCallback`;
+  const wikiRequestTimeout = setTimeout(() => {
+    $wikiElem.text('failed to get wikipedia resources');
+  }, 8000);
 
-    // load wikipedia data
+  $.ajax({
+    url: wikiUrl,
+    dataType: 'jsonp',
+    jsonp: 'callback',
+    success(response) {
+      const articleList = response[1];
 
-    // YOUR CODE GOES HERE!
+      for (let i = 0; i < articleList.length; i++) {
+        const articleStr = articleList[i];
+        const url = `http://en.wikipedia.org/wiki/${articleStr}`;
+        $wikiElem.append(`<li><a href="${url}">${articleStr}</a></li>`);
+      }
 
-    return false;
-};
+      clearTimeout(wikiRequestTimeout);
+    },
+  });
+  // YOUR CODE GOES HERE!
+
+  return false;
+}
 
 $('#form-container').submit(loadData);
 
